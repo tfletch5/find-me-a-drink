8ed00e359903a8537c1d81cad7b9cada534d7faa
Tony Fletcher <tfletch55@gmail.com>
2023-05-15 23:35:36 -0400

    Ran Prettier

--- app/Body.tsx
+++ app/Body.tsx
@@ -1,24 +1,22 @@
-"use client";
+'use client'
 import { useEffect } from 'react'
 
 export default function Body() {
     useEffect(() => {
-
         fetch('/api/data')
-            .then(res => {
+            .then((res) => {
                 if (res.ok) {
                     return res.json()
                 }
             })
-            .then(data => {console.log('see my data', data); return data})
-            .catch(e => {
+            .then((data) => {
+                console.log('see my data', data)
+                return data
+            })
+            .catch((e) => {
                 console.log('THIS IS A ERROR')
             })
     }, [])
 
-    return (
-        <div>
-            damn
-        </div>
-    )
+    return <div>damn</div>
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,18 +4,18 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Find me a Drink',
-  description: 'Use your zip code to find the perfect drink near you.',
+    title: 'Find me a Drink',
+    description: 'Use your zip code to find the perfect drink near you.',
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    )
 }

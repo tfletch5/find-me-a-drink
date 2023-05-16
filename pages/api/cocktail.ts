ad8d482f317d7ce1514f04a0244459e0d2e2c992
Tony Fletcher <tfletch55@gmail.com>
2023-05-15 23:31:35 -0400

    Implemented Cocktail API

---
+++ pages/api/cocktail.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+  const options = {
+    method: "GET",
+    url: "https://the-cocktail-db.p.rapidapi.com/search.php",
+    params: { s: "tequila" },
+    headers: {
+      "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
+      "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const {data} = await axios.request(options);
+    const results = {
+      count: data.drinks.length,
+      drinks: data.drinks,
+    };
+    
+    res.status(200).json(results);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+}
